51fa009c10a773f005c038b63765b18a1b213120
Joseph Hostyk <jhostyk@college.harvard.edu>
2016-02-17 14:50:49 -0500

    pseudocode

---
+++ cs124pset3.py
@@ -0,0 +1,29 @@
+### CS 124 Assignment 3
+
+### February 22, 2016
+
+### Joseph Hostyk, Ryan Kerr
+
+
+# Create a new x containing only the element x.
+def makeset(x): 
+
+#  Given an element x, which set does it belong to?
+def find(x):
+
+# Replace the set containing x and the set containing y by their union.
+def union(x,y): 
+
+
+# Create a dims-dimension graph with n nodes:
+def create_graph(n, dims):
+
+for 
+
+
+
+visitedVertices =[]
+when length(vistied )= n
+we're done
+
+# randnum generator: 100000000.0/(struct.unpack("<L", os.urandom(4))[0])
